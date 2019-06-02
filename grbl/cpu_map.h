d508157b264369235d0882975be3c9ca197ed522
Ingmar Joern <50134155+ingeninge@users.noreply.github.com>
2019-06-02 20:22:33 +0200

    Added #include cpu_map file M2560

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -26,6 +26,9 @@
 #ifndef cpu_map_h
 #define cpu_map_h
 
+#ifdef CPU_MAP_ATMEGA2560 // (Arduino Mega 2560) Working @EliteEng
+  #include "cpu_map/cpu_map_atmega2560.h"
+#endif
 
 #ifdef CPU_MAP_ATMEGA328P // (Arduino Uno) Officially supported by Grbl.
 
